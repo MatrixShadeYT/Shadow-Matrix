258ab0bd00b3415e604221d7e0041b1fbcbed0df
MatrixShadeYT <mpro9234@gmail.com>
2025-04-05 17:35:53 -0600

    innotation +f1

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,6 +1,6 @@
 import pyttsx3
 engine = pyttsx3.init()
-engine.setProperty('voice', engine.getProperty('voices')[1].id)
+engine.setProperty('voice', engine.getProperty('voices')[1].id+'+f1')
 def speech(text):
     engine.say(text)
     engine.runAndWait()
