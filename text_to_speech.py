031115e917765e43ce8ba0aad1aeb342c359912e
MatrixShadeYT <mpro9234@gmail.com>
2025-04-05 17:49:14 -0600

    .

--- text_to_speech.py
+++ text_to_speech.py
@@ -3,20 +3,28 @@ engine = pyttsx3.init()
 def speech(text):
     if (text.lower().replace(' ','') == "0+f1"):
         engine.setProperty('voice', engine.getProperty('voices')[0].id+'+f1')
+        print('tone: 0, +f1')
     elif (text.lower().replace('plus ','+') == "0+f2"):
         engine.setProperty('voice', engine.getProperty('voices')[0].id+'+f2')
+        print('tone: 0, +f2')
     elif (text.lower().replace('plus ','+') == "0+f3"):
         engine.setProperty('voice', engine.getProperty('voices')[0].id+'+f3')
+        print('tone: 0, +f3')
     elif (text.lower().replace('plus ','+') == "0+f4"):
         engine.setProperty('voice', engine.getProperty('voices')[0].id+'+f4')
+        print('tone: 0, +f4')
     elif (text.lower().replace('plus ','+') == "1+f4"):
         engine.setProperty('voice', engine.getProperty('voices')[1].id+'+f1')
+        print('tone: 1, +f1')
     elif (text.lower().replace('plus ','+') == "1+f2"):
         engine.setProperty('voice', engine.getProperty('voices')[1].id+'+f2')
+        print('tone: 1, +f2')
     elif (text.lower().replace('plus ','+') == "1+f3"):
         engine.setProperty('voice', engine.getProperty('voices')[1].id+'+f3')
+        print('tone: 1, +f3')
     elif (text.lower().replace('plus ','+') == "1+f4"):
         engine.setProperty('voice', engine.getProperty('voices')[1].id+'+f4')
+        print('tone: 1, +f4')
 
     engine.say(text)
     engine.runAndWait()
