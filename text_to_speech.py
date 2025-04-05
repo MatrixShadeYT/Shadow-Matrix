ea2f81c2f04096be623981783e207464d725162c
MatrixShadeYT <mpro9234@gmail.com>
2025-04-05 17:59:16 -0600

    .

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,13 +1,13 @@
 import pyttsx3
 engine = pyttsx3.init()
 def speech(text):
-    if (text.lower().replace(' ','') == "0+f1"):
+    if ("0+f1" in text.lower().replace(' ','')):
         engine.setProperty('voice', engine.getProperty('voices')[0].id+'+f1')
         print('tone: 0+f1')
-    if (text.lower() == "boy"):
+    if ("boy" in text.lower()):
         engine.setProperty('voice', engine.getProperty('voices')[0].id)
         print('tone: 0')
-    if (text.lower() == "girl"):
+    if ("girl" in text.lower()):
         engine.setProperty('voice', engine.getProperty('voices')[1].id)
         print('tone: 1')
     engine.say(text)
