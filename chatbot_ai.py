6f040f9b0b1b03d1dbc2738a8ef46effd506c24d
MatrixShadeYT <mpro9234@gmail.com>
2025-04-05 18:56:10 -0600

    conversation

--- chatbot_ai.py
+++ chatbot_ai.py
@@ -1,2 +1,13 @@
-def response(text):
+conversation = {
+    "user": [],
+    "bot": []
+}
+
+def message(text):
     return "Response."
+
+def response(text):
+    output = message(text)
+    conversation['user'].append(text)
+    conversation['bot'].append(output)
+    return output
