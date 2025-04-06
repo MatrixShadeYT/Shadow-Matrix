f5535fa1cb325b82eb2f4387f5eab81b8174dd30
MatrixShadeYT <mpro9234@gmail.com>
2025-04-05 18:40:25 -0600

    .

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from speech_to_text import microphone
 from text_to_speech import speech
 #import pyaudio
 import time
-print('Program started.')
+print('\nProgram started.\n')
 speech('Program started.')
 while(True):
     mic = microphone()
     if (mic == "stop"):
-        print('System: COMMAND STOP DETECTED!\nExiting program.')
+        print('System: COMMAND STOP DETECTED!')
         speech('Exiting program.')
+        print('\nExiting program.')
         break
     response = "Response."
     print("ME: {0}".format(mic))
