b2cdb5dd2952dd9a158e6277abc7f6ac88279938
MatrixShadeYT <mpro9234@gmail.com>
2025-04-05 18:01:55 -0600

    girl voice

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,14 +1,6 @@
 import pyttsx3
 engine = pyttsx3.init()
+engine.setProperty('voice', engine.getProperty('voices')[1].id)
 def speech(text):
-    if ("0+f1" in text.lower().replace(' ','')):
-        engine.setProperty('voice', engine.getProperty('voices')[0].id+'+f1')
-        print('tone: 0+f1')
-    if ("boy" in text.lower()):
-        engine.setProperty('voice', engine.getProperty('voices')[0].id)
-        print('tone: 0')
-    if ("girl" in text.lower()):
-        engine.setProperty('voice', engine.getProperty('voices')[1].id)
-        print('tone: 1')
     engine.say(text)
     engine.runAndWait()
