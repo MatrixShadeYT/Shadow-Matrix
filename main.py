86ab10310138e8118dc2ba6dc2a034bacef65ddb
MatrixShadeYT <mpro9234@gmail.com>
2025-04-05 18:29:53 -0600

    .

--- main.py
+++ main.py
@@ -2,14 +2,12 @@ from speech_to_text import microphone
 from text_to_speech import speech
 #import pyaudio
 import time
-time.sleep(1)
-speech('Program started.')
 print('Program started.')
+speech('Program started.')
 while(True):
     mic = microphone()
     if (mic == "stop"):
+        print('Exiting program.')
+        speech('Exiting program.')
         break
     print("ME: {0}".format(mic))
-time.sleep(1)
-speech('Exiting program.')
-print('Exiting program.')
