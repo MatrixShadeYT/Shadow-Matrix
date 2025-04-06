5f16735197a004afe37183bee319107668ce17da
MatrixShadeYT <mpro9234@gmail.com>
2025-04-05 18:20:32 -0600

    .

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from speech_to_text import microphone
 from text_to_speech import speech
 #import pyaudio
+speech('Program started.')
 while(True):
-    speech('Ready.')
     mic = microphone()
-    speech('Processing.')
     if (mic == "stop"):
         speech('Exiting program.')
         break
