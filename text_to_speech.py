0616d6d4df30c77fc66cdc6549f05f323562f98d
MatrixShadeYT <mpro9234@gmail.com>
2025-04-05 18:08:25 -0600

    girl voice

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,6 +1,6 @@
 import pyttsx3
 engine = pyttsx3.init()
-engine.setProperty('voice', engine.getProperty('voices')[0].id)
+engine.setProperty('voice', engine.getProperty('voices')[1].id)
 def speech(text):
     engine.say(text)
     engine.runAndWait()
