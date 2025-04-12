697e9e1ed2a8810b3d4e5cd2deecbfb6c50fae16
MatrixShadeYT <mpro9234@gmail.com>
2025-04-12 12:14:31 -0600

    create table

--- database.py
+++ database.py
@@ -1,3 +1,9 @@
 import sqlite3
 
 connection = sqlite3.connect('ShadowAI.db')
+cursor = connection.cursor()
+
+c.execute('''CREATE TABLE dataset (
+          input text,
+          output text,
+)''')
