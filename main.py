b9435dfedcd32acb4f729c0f626cabde07f66c3a
MatrixShadeYT <mpro9234@gmail.com>
2025-04-12 14:11:42 -0600

    moving stt format to file

--- main.py
+++ main.py
@@ -6,20 +6,12 @@ speech('Program started')
 print('BOT: Program started.')
 
 while True:
-    mic = microphone().lower()
-    if mic == "exit program":
+    mic = microphone()
+    if mic == "Exit program.":
         print('ME: Exit program.')
         speech('Exiting program.')
         print('BOT: Exiting program.')
         break
-    question = False
-    for i in ['who','what','where','why','when','how']:
-        if i in mic:
-            question = True
-    if question == True:
-        print("ME: {0}".format(mic[0].upper()+mic[1:]+"?"))
-    else:
-        print("ME: {0}".format(mic[0].upper()+mic[1:]+"."))
     response = chatbot_ai.response(mic)
     speech(response.replace('\n',' '))
     print("BOT: {0}".format(response))
--- speech_to_text.py
+++ speech_to_text.py
@@ -2,6 +2,17 @@ import speech_recognition as sr
 
 r = sr.Recognizer()
 
+def formatText(text):
+    text = text.lower()
+    question = False
+    for i in ['who','what','where','why','when','how']:
+        if i in text:
+            question = True
+    if question == True:
+        return '{0}'.format(text[0].upper()+text[1:]+"?")
+    else:
+        return '{0}'.format(text[0].upper()+text[1:]+".")
+
 def microphone():
     while True:
         try:
@@ -9,7 +20,8 @@ def microphone():
                 r.adjust_for_ambient_noise(source2, duration=0.1)
                 audio2 = r.listen(source2)
                 MyText = r.recognize_google(audio2)
-                return MyText
+                text = format(MyText)
+                return text
         except sr.RequestError as e:
             print("Could not request results; {0}".format(e))
         except sr.UnknownValueError:
