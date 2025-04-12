f576c3d8a19228dc7cfe1b21e666cf1bbd3d5ca8
MatrixShadeYT <mpro9234@gmail.com>
2025-04-12 13:26:45 -0600

    enter function

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ import os
 connection = sqlite3.connect(':memory:')
 cursor = connection.cursor()
 
-cursor.execute('''CREATE TABLE conversation (
+cursor.execute('''CREATE TABLE dataset (
     input text,
     output text
 )''')
@@ -13,8 +13,8 @@ connection.commit()
 def previous():
     pass
 
-def input():
-    pass
+def enter(inputed, outputed):
+    cursor.execute("INSERT INTO dataset VALUES (:input, :output)", {'input': inputed, 'output': outputed})
 
 def close():
     connection.close()
