71b3bd7f647d5048d924b1f32b50e3320795a1cf
MatrixShadeYT <mpro9234@gmail.com>
2025-04-12 13:34:21 -0600

    removeItem function

--- database.py
+++ database.py
@@ -10,12 +10,23 @@ cursor.execute('''CREATE TABLE dataset (
 )''')
 connection.commit()
 
-def previous():
-    pass
+def getList():
+    cursor.execute("SELECT * FROM dataset")
+    return cursor.fetchall()
 
 def enter(inputed, outputed):
     with connection:
-        cursor.execute("INSERT INTO dataset VALUES (:input, :output)", {'input': inputed, 'output': outputed})
+        cursor.execute(
+            "INSERT INTO dataset VALUES (:input, :output)",
+            {'input': inputed, 'output': outputed}
+        )
+
+def removeItem(inputed,outputed):
+    with connection:
+        cursor.execute(
+            "DELETE from dataset WHERE input = :input AND output = :output",
+            {'input': inputed,'output',outputed}
+        )
 
 def close():
     connection.close()
