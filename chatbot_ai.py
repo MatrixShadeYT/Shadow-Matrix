98b15f89fa70bd173a6bed7cd6861cd9124c87d7
MatrixShadeYT <mpro9234@gmail.com>
2025-04-25 14:18:42 -0600

    Text to Number

--- chatbot_ai.py
+++ chatbot_ai.py
@@ -1,6 +1,14 @@
 import database
+import string
 import keras
 
+tokenizer = list(' ,.!?*:'+string.ascii_lowercase)
+formatting = [
+    ['and','&'],
+    ['dollar','$'],
+    ['percent','%'],
+    ['hashtag','#']
+]
 data = database.dataset()
 model = keras.Sequential([
     keras.Input(shape=(180)),
@@ -19,12 +27,27 @@ model.fit(
     [ # Output
         f"{i[2]}" for i in data.getList()
     ],
-    epochs=10
+    epochs=100
 )
 
+def textTokenizer(text):
+    x = ''
+    for i in text:
+        x = f'{x}{tokenizer.index(i)}'
+    for i in formatting:
+        x.replace(i[0],i[1])
+    return x
+
+def textualize(text):
+    x = ''
+    for i in text:
+        x = f'{x}{tokenizer[i]}'
+    return x
+
 def request(text,user):
-    output = ""
-    return output
+    text = textTokenizer(f'{user}:{text}')
+    output = model.predict(text,batch_size=1,verbose=0)
+    return textualize(output)
 
 def response(text,user):
     output = request(text,user)
