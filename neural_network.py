9737aab8e70d3d24fa51b5c2bb33a0d0e32eab1a
MatrixShadeYT <mpro9234@gmail.com>
2025-04-25 23:31:12 -0600

    Don't understand

--- neural_network.py
+++ neural_network.py
@@ -1,14 +1,6 @@
 from random import randint
 import numpy as np
 
-class Loss:
-    def calculate(self,output,y):
-        return np.mean(self.forward(output,y))
-
-class LCC(Loss):
-    def forward(self,y_pred,y_true):
-        return -np.log(np.clip(y_pred,1e-7,1-1e-7)[range(len(y_pred)),y_true] if len(y_true.shape) == 1 elif len(y_true.shape) == 2: np.sum(np.clip(y_pred,1e-7,1-1e-7)*y_true,axis=1))
-
 # Layer Class
 class Layer_Dense:
     def __init__(self,inputs=1,data=5):
