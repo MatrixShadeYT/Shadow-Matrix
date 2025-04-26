552771260be763f8e6be0edb68b697f30a6721a3
MatrixShadeYT <mpro9234@gmail.com>
2025-04-25 22:32:21 -0600

    Simplify Numpy to NP

--- neural_network.py
+++ neural_network.py
@@ -1,16 +1,16 @@
 from random import randint
-import numpy
+import numpy as np
 
 # Layer Class
 class Layer_Dense:
     def __init__(self,inputs=1,data=5):
         self.activation = activation
         if isinstance(data,int):
-            self.weights = 0.1*numpy.random.randn(inputs,data)
-            self.biases = numpy.zeros((1,data))
+            self.weights = 0.1*np.random.randn(inputs,data)
+            self.biases = np.zeros((1,data))
         else:
             self.biases, self.weights = data
     def forward(self,inputs):
         self.output = np.dot(inputs,self.weights)+self.biases
         self.ReLu = np.maximum(0,self.output)
-        self.SM = numpy.exp(self.output-np.max(self.output,axis=1,keepdims=True)) / numpy.sum(numpy.exp(self.output-np.max(self.output,axis=1,keepdims=True)),axis=1,keepdims=True)
+        self.SM = np.exp(self.output-np.max(self.output,axis=1,keepdims=True)) / np.sum(np.exp(self.output-np.max(self.output,axis=1,keepdims=True)),axis=1,keepdims=True)
