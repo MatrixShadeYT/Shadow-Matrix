00db77f5045a08af40e5e3e242da2d002b4f0a1c
MatrixShadeYT <mpro9234@gmail.com>
2025-04-25 20:53:45 -0600

    Euler's Number

--- neural_network.py
+++ neural_network.py
@@ -1,11 +1,8 @@
 from random import randint
 import numpy
 
-X = [
-    [1.0,2.0,3.0,2.5],
-    [2.0,5.0,-1.0,2.0],
-    [-1.5,2.7,3.3,-0.8]
-]
+#Euler's Number
+e = 2.718281182846
 
 # Layer Class
 class Layer_Dense:
