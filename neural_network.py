b5eca71f023271723bb76f242423dd9b2dc9caea
MatrixShadeYT <mpro9234@gmail.com>
2025-04-25 20:29:01 -0600

    ReLu Activation

--- neural_network.py
+++ neural_network.py
@@ -8,11 +8,15 @@ X = [
 ]
 
 class layer:
-    def __init__(self,inputs=1,data=5):
+    def __init__(self,inputs=1,data=5,activation):
+        self.activation = activation
         if isinstance(data,int):
             self.weights = 0.1*numpy.random.randn(inputs,data)
             self.biases = numpy.zeros((1,data))
         else:
             self.biases, self.weights = data
     def forward(self,inputs):
-        self.output = np.dot(inputs,self.weights)+self.biases
+        if self.activation == 'ReLu':
+            self.output = max(0,np.dot(inputs,self.weights)+self.biases)
+        else:
+            self.output = np.dot(inputs,self.weights)+self.biases
