87a2800a1afacca32f5cf44a69bc51786cd1fa45
MatrixShadeYT <mpro9234@gmail.com>
2025-04-25 18:34:44 -0600

    Generate WB

--- neural_network.py
+++ neural_network.py
@@ -3,7 +3,13 @@ import numpy
 
 class layer:
     def __init__(self,data):
-        self.biases, self.weights = data
+        if data:
+            self.biases, self.weights = data
+        else:
+            self.weights = []
+            for i in range(data):
+                self.weights.append([randint(0,100)/100 for i in range(data)])
+            self.biases = [randint(0,100)/100 for i in range(data)]
     def output(self,inputs):
         return numpy.dot(self.weights*inputs) + self.biases
     def getData(self):
