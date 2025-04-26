4e9894b0a1c42cd6bb4013bd36688b48ee627520
MatrixShadeYT <mpro9234@gmail.com>
2025-04-25 20:06:14 -0600

    NNfS

--- neural_network.py
+++ neural_network.py
@@ -1,28 +1,16 @@
 from random import randint
 import numpy
 
-class layer:
-    def __init__(self,data):
-        if isinstance(data,list):
-            self.biases, self.weights = data
-        else:
-            self.weights = []
-            for i in range(data):
-                self.weights.append([randint(0,100)/100 for i in range(data)])
-            self.biases = [randint(0,100)/100 for i in range(data)]
-    def output(self,inputs):
-        return numpy.dot(self.weights*inputs) + self.biases
-    def neuron(self,num,bias,weights):
-        self.weights[num] = weights
-        self.baises[num] = bias
-    def getData(self):
-        return self.neurons
+X = [
+    [1.0,2.0,3.0,2.5],
+    [2.0,5.0,-1.0,2.0],
+    [-1.5,2.7,3.3,-0.8]
+]
 
-class model:
-    def __init__(self,layers):
-        self.layers = layers
-    def output(self,inputs):
-        inputs = inputs
-        for i in range(len(self.layers)):
-            inputs = self.layers[i].output(inputs)
-        return inputs
+class layer:
+    def __init__(self,inputs=1,data=5):
+        if isinstance(data,int):
+            self.weights = 0.1*numpy.random.randn(inputs,data)
+            self.biases = numpy.zeros((1,data))
+        else:
+            self.biases, self.weights = data
