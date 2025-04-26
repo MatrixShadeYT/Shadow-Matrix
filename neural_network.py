79286649ecbfd0909e31a517af225bb746790918
MatrixShadeYT <mpro9234@gmail.com>
2025-04-25 18:27:41 -0600

    Making into layer

--- neural_network.py
+++ neural_network.py
@@ -1,42 +1,10 @@
 from random import randint
 import numpy
 
-# Data = [BIAS,[WEIGHTS]]
-class neuron:
-    def __init__(self,data):
-        if isinstance(data,int):
-            self.weights = [randint(0,100)/100 for i in range(data)]
-            self.bias = randint(0,100)/100
-        else:
-            self.weights = data[1]
-            self.bias = data[0]
-    def output(self,inputs):
-        output = self.bias
-        for i in range(len(inputs)):
-            output += inputs[i]*self.weights[i]
-        return output
-    def getData(self):
-        return [self.bias,self.weights]
-
-# Layer [NEURONS] or [[BIAS,[WEIGHTS]]]
 class layer:
     def __init__(self,data):
-        if isinstance(data[0],int):
-            self.neurons = [neuron(data[i]) for i in range(len(data))]
-        else:
-            self.neurons = data
+        self.biases, self.weights = data
     def output(self,inputs):
-        if isinstance(self.neurons,list):
-            output = [self.neurons[i][0] for i in range(len(self.neurons))]
-            for i in range(len(self.neurons)):
-                for x in range(len(inputs)):
-                    output[i] += inputs[x]*self.neurons[i][1][x]
-        else:
-            output = [self.neurons[i].getData() for i in range(len(self.neurons))]
-        return output
+        return numpy.dot(self.weights*inputs) + self.biases
     def getData(self):
-        if isinstance(self.neurons,list):
-            output = self.neurons
-        else:
-            [self.neurons[i].getData() for i in range(len(self.neurons))]
-        return output
+        return self.neurons
