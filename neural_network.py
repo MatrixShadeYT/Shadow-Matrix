6872c8c7ddbb2a3a2bd8e8e0a0b9ff1e6d4b5572
MatrixShadeYT <mpro9234@gmail.com>
2025-04-25 22:23:49 -0600

    Simplify

--- neural_network.py
+++ neural_network.py
@@ -3,7 +3,7 @@ import numpy
 
 # Layer Class
 class Layer_Dense:
-    def __init__(self,inputs=1,data=5,activation=0):
+    def __init__(self,inputs=1,data=5):
         self.activation = activation
         if isinstance(data,int):
             self.weights = 0.1*numpy.random.randn(inputs,data)
@@ -14,4 +14,3 @@ class Layer_Dense:
         self.output = np.dot(inputs,self.weights)+self.biases
         self.ReLu = np.maximum(0,self.output)
         self.SM = numpy.exp(self.output-np.max(self.output,axis=1,keepdims=True)) / numpy.sum(numpy.exp(self.output-np.max(self.output,axis=1,keepdims=True)),axis=1,keepdims=True)
-        
