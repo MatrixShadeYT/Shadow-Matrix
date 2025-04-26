bf82b37343364d548dc9774080b624c8ec8eec9f
MatrixShadeYT <mpro9234@gmail.com>
2025-04-25 23:56:14 -0600

    Make Model

--- neural_network.py
+++ neural_network.py
@@ -1,9 +1,18 @@
 from random import randint
 import numpy as np
 
+class model:
+    def __init__(self,layers):
+        self.layers = layers
+    def output(self,inputs):
+        inputs = inputs
+        for i in range(len(self.layers)):
+            inputs = self.layers[i].forward(inputs)
+        return inputs
+
 # Layer Class
 class Layer_Dense:
-    def __init__(self,inputs=1,data=5):
+    def __init__(self,inputs=1,data=5,activation=0):
         self.activation = activation
         if isinstance(data,int):
             self.weights = 0.1*np.random.randn(inputs,data)
@@ -14,3 +23,9 @@ class Layer_Dense:
         self.output = np.dot(inputs,self.weights)+self.biases
         self.ReLu = np.maximum(0,self.output)
         self.SM = np.exp(self.output-np.max(self.output,axis=1,keepdims=True)) / np.sum(np.exp(self.output-np.max(self.output,axis=1,keepdims=True)),axis=1,keepdims=True)
+        if self.activation == 'ReLu':
+            return self.ReLu
+        elif self.activation == 'SM':
+            return self.SM
+        else:
+            return self.output
