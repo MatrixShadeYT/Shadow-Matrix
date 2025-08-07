1f03e416c8ab9cb905e5b494d27979d160ba98e7
MatrixShadeYT <mpro9234@gmail.com>
2025-08-07 00:12:58 -0600

    Fine text

--- main.py
+++ main.py
@@ -3,23 +3,16 @@ from text_to_speech import speech
 import chatbot_ai
 
 speech('Program started')
-print('BOT: Program started.')
-
-def textualize(text):
-    if ['who','what','where','why','when','how'] in text.split(' '):
-        text = f"{text}?"
-    else:
-        text = f"{text}."
-    return text.capitalize()
+print('System: Program started.')
 
 while True:
-    mic = textualize(microphone())
+    mic = f"{microphone().capitalize()}."
     if mic == "Exit program.":
         print('Shade: Exit program.')
         speech('Exiting program.')
-        print('BOT: Exiting program.')
+        print('System: Exiting program.')
         break
     print(f'Shade: {mic}')
     response = chatbot_ai.response(mic,'Shade')
     speech(response.replace('\n',' '))
-    print("BOT: {0}".format(response))
+    print("Shadow: {0}".format(response))
