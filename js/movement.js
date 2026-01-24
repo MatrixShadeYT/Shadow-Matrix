cf1592cf150ccbd2ee14c093cf50f1c695f50391
MatrixShadeYT <mpro9234@gmail.com>
2026-01-24 09:44:25 -0700

    base

--- js/movement.js
+++
@@ -1,7 +0,0 @@
-export class Movement {
-    constructor({position,limits}) {
-        this.position = position ?? {x: 0, y: 0, z: 0};
-        this.limits = limits ?? {x: 0, y: 0};
-    }
-    updatePosition(deltaPosition,priority) {}
-}
--- main.js
+++ main.js
@@ -1,41 +1,9 @@
-import { Movement } from './js/movement.js';
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 canvas.height = canvas.width*(9/16);
 canvas.width = canvas.width;
-
-const head = new Movement({
-    position: {
-        x: canvas.width/2,
-        y: canvas.height/2,
-        z: 0
-    },
-    limits: {x: 100, y: 100}
-})
-const body = new Movement({
-    position: {
-        x: canvas.width/2,
-        y: canvas.height/2 + 50,
-        z: 0
-    },
-    limits: {x: 100, y: 100}
-});
-
-document.addEventListener('mousemove', (event) => {
-    console.log(event.clientX, event.clientY);
-    body.updatePosition({
-        x: event.clientX,
-        y: event.clientY
-    });
-},false);
-
 function animate() {
-    //window.requestAnimationFrame(animate);
+    window.requestAnimationFrame(animate);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.beginPath();
-    ctx.moveTo(head.position.x,head.position.y);
-    ctx.lineTo(body.position.x,body.position.y);
-    ctx.stroke();
-    ctx.closePath();
 }
 window.requestAnimationFrame(animate);
