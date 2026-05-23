93779dd88cc6feb9de8823f7f355579e3f77a5e6
MatrixShadeYT <mpro9234@gmail.com>
2026-05-23 17:26:01 -0600

    Add Hiyori model and icon assets
    - Created hiyori.vtube.json configuration file for the Hiyori model, including settings for animations, parameters, and hotkeys.
    - Added icon.jpg for the Hiyori model representation.

--- main.js
+++
@@ -1,9 +0,0 @@
-const canvas = document.getElementById('gameCanvas');
-const ctx = canvas.getContext('2d');
-canvas.height = canvas.width*(9/16);
-canvas.width = canvas.width;
-function animate() {
-    window.requestAnimationFrame(animate);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
-window.requestAnimationFrame(animate);
